a1c6e2a5d9dc289f8aa4b20d212f23cd7ceafd28
Well Luiz <wellwlds@hotmail.com>
2023-02-15 22:16:58 -0300

    Organização de código, layout e criando balão para leite de hoje

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:intl/intl.dart';
 
@@ -15,13 +14,14 @@ class AddNote extends StatefulWidget {
 }
 
 class AddNoteState extends State<AddNote> {
-  var rng = Random();
   var k;
   var ref;
   final fb = FirebaseDatabase.instance;
 
   late DatabaseReference todos = fb.ref().child(widget.title.toLowerCase());
   int totalDia = 0;
+  int totalHoje = 0;
+  int totalPlantao = 0;
 
   TextEditingController qtdController = TextEditingController();
   TextEditingController dateController = TextEditingController();
@@ -42,8 +42,7 @@ class AddNoteState extends State<AddNote> {
   }
 
   openModal(type) {
-    rng = Random();
-    k = rng.nextInt(10000);
+    k = getDateTimeId();
     ref = fb.ref().child('${widget.title.toLowerCase()}/$k');
     showDialog(
         context: context,
@@ -64,6 +63,7 @@ class AddNoteState extends State<AddNote> {
                       child: TextField(
                         controller: qtdController,
                         textAlign: TextAlign.center,
+                        keyboardType: type.toLowerCase() == "leite" || type.toLowerCase() == "febre" ? TextInputType.number : TextInputType.text,
                         decoration: const InputDecoration(
                           hintText: 'Quantidade',
                         ),
@@ -75,8 +75,6 @@ class AddNoteState extends State<AddNote> {
                     MaterialButton(
                       color: Colors.cyan,
                       onPressed: () {
-                        print(type.toLowerCase());
-                        print(type.toLowerCase() == "leite");
                         if(type.toLowerCase() == "leite") {
                           sumCounter(int.parse(qtdController.text));
                         }
@@ -185,20 +183,29 @@ class AddNoteState extends State<AddNote> {
           );
         });
   }
-
+  int verificaPlantao(DateTime data) {
+    if (data.hour >= 07 && data.hour < 19) {
+      return 1;
+    }
+    
+    return 2;
+  }
   getCounter() async {
-    totalDia = totalDia + 1;
     todos.once().then((snapshot){
     Map<dynamic, dynamic> values = snapshot.snapshot.value as Map<dynamic, dynamic>;    
-    print(values);
     values.forEach((key, value) {
       DateTime data = DateTime.parse(value['datetime'].toString());
       DateTime dataInicial = DateTime.parse(DateTime.now().subtract(const Duration(days: 1)).toString());
       DateTime dataFinal = DateTime.parse(DateTime.now().toString());
-      if (dataInicial.isBefore(data) && dataFinal.isAfter(data)) {
-        if (value['type'] == "leite") {
+      if (value['type'] == "leite") {
+        if (dataInicial.isBefore(data) && dataFinal.isAfter(data)) {
           setState(() {
             totalDia += int.parse(value['qtd']);
+          });
+        }
+        if (data.day == dataFinal.day && dataFinal.month == data.month) {
+          setState(() {
+            totalHoje += int.parse(value["qtd"]);
           });
         }
       }
@@ -207,7 +214,6 @@ class AddNoteState extends State<AddNote> {
   }
 
   sumCounter(int value) {
-    print(totalDia.toString() + ' + ' + value.toString());
     setState(() {
       totalDia += value;
     });
@@ -219,18 +225,20 @@ class AddNoteState extends State<AddNote> {
     }
     return "";
   }
+
+  int getDateTimeId() {
+    DateTime date = DateTime.now();
+    String dateWorkers = date.year.toString() + date.month.toString().padLeft(2, '0') + date.day.toString().padLeft(2, '0')  + date.hour.toString().padLeft(2, '0') + date.minute.toString() + date.second.toString().padLeft(2, '0');
+    return int.parse(dateWorkers);
+  }
   @override
   void initState() {
     getCounter();
+    todos = fb.ref().child(widget.title.toLowerCase());
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    rng = Random();
-    k = rng.nextInt(10000);
-    ref = fb.ref().child('${widget.title.toLowerCase()}/$k');
-    todos = fb.ref().child(widget.title.toLowerCase());
-
     return Scaffold(
       backgroundColor: Colors.black12,
       appBar: AppBar(
@@ -276,73 +284,109 @@ class AddNoteState extends State<AddNote> {
       body: Column(
         children: [
           Flexible(
-            flex: 1,
-            child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(150)),
-                      border: Border.all(
-                        width: 3,
-                        color: Colors.black,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
+            flex: 2,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(10.0),
                     child: Center(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 10),
-                          Text(totalDia.toString(),style: const TextStyle(fontSize: 50)),
-                          Text("Leite/24h"),
-                        ],
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(150)),
+                          border: Border.all(
+                            width: 3,
+                            color: Colors.black,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        child: Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(totalDia.toString(),style: const TextStyle(fontSize: 50)),
+                              const Text("Leite/24h"),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                    )
                   ),
-                )
-              )
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Center(
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(150)),
+                          border: Border.all(
+                            width: 3,
+                            color: Colors.black,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(totalHoje.toString(),style: const TextStyle(fontSize: 50)),
+                              const Text("Leite hoje"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  )
+              ],
+            )
           ),
           Flexible(
             flex: 4,
-            child: FirebaseAnimatedList(
-              key: const ValueKey<bool>(false),
-              query: todos,
-              reverse: false,
-              itemBuilder: (context, snapshot, animation, index) {
-                Map<dynamic, dynamic> values =
-                    snapshot.value as Map<dynamic, dynamic>;
+            child: Padding(
+              padding: const EdgeInsets.only(bottom:50.0),
+              child: FirebaseAnimatedList(
+                key: const ValueKey<bool>(false),
+                query: todos,
+                reverse: true,
+                itemBuilder: (context, snapshot, animation, index) {
+                  Map<dynamic, dynamic> values =
+                      snapshot.value as Map<dynamic, dynamic>;
 
-                DateTime data = DateTime.parse(values['datetime'].toString());
+                  DateTime data = DateTime.parse(values['datetime'].toString());
 
-                if (dateController.text.length > 4) {
-                  if (pickedDate!.day != data.day ||
-                      pickedDate!.month != data.month ||
-                      pickedDate!.year != data.year) {
-                    return const SizedBox();
+                  if (dateController.text.length > 4) {
+                    if (pickedDate!.day != data.day ||
+                        pickedDate!.month != data.month ||
+                        pickedDate!.year != data.year) {
+                      return const SizedBox();
+                    }
                   }
-                }
-                return SizeTransition(
-                  sizeFactor: animation,
-                  child: ListTile(
-                    trailing: IconButton(
-                      onPressed: () {
-                        sumCounter(int.parse(values['qtd']) * (-1));
-                        todos.child(snapshot.key!).remove();
-                      },
-                      icon: const Icon(Icons.delete),
+                  return SizeTransition(
+                    sizeFactor: animation,
+                    child: ListTile(
+                      trailing: IconButton(
+                        onPressed: () {
+                          todos.child(snapshot.key!).remove();
+                          if(values['type'] == 'leite') sumCounter(int.parse(values['qtd']) * (-1));
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                      leading: Icon(
+                        getIcon(values['type']),
+                        color: Colors.red,
+                        size: 30,
+                      ),
+                      title: Text(
+                          '${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.parse(values['datetime']))}: ${values['qtd']}${getMetric(values['type'])}',style: TextStyle(fontSize: 20),),
                     ),
-                    leading: Icon(
-                      getIcon(values['type']),
-                      color: Colors.red,
-                      size: 30,
-                    ),
-                    title: Text(
-                        '${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.parse(values['datetime']))}: ${values['qtd']}${getMetric(values['type'])}',style: TextStyle(fontSize: 20),),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ],
